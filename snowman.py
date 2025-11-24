fd5b44b18a2bce9bb2a2f92da7105555a31ba154
whacktor <lammelviktor2@gmail.com>
2025-11-24 17:01:44 +0100

    added Stages

--- snowman.py
+++ snowman.py
@@ -1,6 +1,36 @@
 #Beta-Version 1.0
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
